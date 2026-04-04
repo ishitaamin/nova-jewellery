504a385101ec559f171d9abddfd3b7608d31aa2c
Ishitaamin <ishitaamin3094@gmail.com>
2026-04-04 21:40:11 +0530

    updated backend url

--- nova-frontend/src/services/api.ts
+++ nova-frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:4000/api";
-export const BACKEND_URL = "http://localhost:4000";
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL + "/api";
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
